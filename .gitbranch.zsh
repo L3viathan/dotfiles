cfd1645e9ff56d3b827df707ce6c9203707eec92
Jonathan Oberländer <Jonathan.Oberlaender@dc1.com>
2019-05-27 14:23:05 +0200

    Fix .gitbranch

--- .gitbranch.zsh
+++ .gitbranch.zsh
@@ -4,7 +4,7 @@ if [[ $- == *i* ]]; then
 # CTRL-B - Paste the selected branch into the command line
 fzf-branch-widget() {
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
-  selected="$(git branch | tr -d '* ' |
+  selected="$(git branch -a | perl -pe 's;remotes/origin/;;' | tr -d '* ' |
     FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --multi --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS +m" $(__fzfcmd))"
   local ret=$?
   LBUFFER=${LBUFFER}${selected}
