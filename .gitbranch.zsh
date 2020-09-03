a659216835f74a327c7bc179e83c60cd60c0ce6c
Jonathan Oberländer <joberlaender@vmray.com>
2020-09-03 10:01:01 +0200

    Make ^B not fail outside a Git repo

--- .gitbranch.zsh
+++ .gitbranch.zsh
@@ -4,12 +4,15 @@ if [[ $- == *i* ]]; then
 # CTRL-B - Paste the selected branch into the command line
 fzf-branch-widget() {
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
-  selected="$(git branch -a | perl -pe 's;remotes/origin/;;' | tr -d '* ' |
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --multi --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS +m" $(__fzfcmd))"
-  local ret=$?
-  LBUFFER=${LBUFFER}${selected}
-  zle reset-prompt
-  return $ret
+  if [ -d .git ] || git rev-parse 2>/dev/null ; then
+      selected="$(git branch -a | perl -pe 's;remotes/origin/;;' | tr -d '* ' |
+        FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --multi --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS +m" $(__fzfcmd))"
+      local ret=$?
+      LBUFFER=${LBUFFER}${selected}
+      zle reset-prompt
+      return $ret
+   fi
+   return 0
 }
 zle     -N   fzf-branch-widget
 bindkey '^B' fzf-branch-widget
