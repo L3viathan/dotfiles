b2ee9418b4512826c7738531d14486d0adb81b88
L3viathan <git@l3vi.de>
2021-02-12 14:35:41 +0100

    Support dbconf parts

--- .dbconf.zsh
+++ .dbconf.zsh
@@ -4,7 +4,7 @@ if [[ $- == *i* ]]; then
 # CTRL-X - Paste the selected db.conf spec into the commandline
 fzf-dbconf-widget() {
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
-  selected="$(grep <$SOLUTE_DEV_ROOT/infrastructure/puppetcfg/modules/packages/files/dbconf/db.conf '^\[' | tr -d '[]' |
+  selected="$(cat $SOLUTE_DEV_ROOT/infrastructure/puppetcfg/modules/packages/files/dbconf/{db.conf,parts/*} | grep '^\[' | tr -d '[]' |
     FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --multi --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS +m" $(__fzfcmd))"
   local ret=$?
   LBUFFER=${LBUFFER}${selected}
